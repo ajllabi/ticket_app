380c35246151372e17c892cb26133e7c13755cf6
Mohamed Ajellabi <dsipdi4@alamana.org.ma>
2026-05-06 23:54:14 +0100

    correct warnings 2

--- lib/screens/search/widgets/app_ticket_tabs.dart
+++ lib/screens/search/widgets/app_ticket_tabs.dart
@@ -12,7 +12,7 @@ class AppTicketTabs extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final size = MediaQuery.of(context).size;
+    //final size = MediaQuery.of(context).size;
 
     return Container(
       decoration: BoxDecoration(
--- lib/screens/search/widgets/find_tickets.dart
+++ lib/screens/search/widgets/find_tickets.dart
@@ -1,4 +1,4 @@
-import 'dart:math';
+//import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:ticket_app/base/res/styles/app_styles.dart';
